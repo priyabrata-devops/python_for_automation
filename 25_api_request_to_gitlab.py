7b976c01aed67654d592fe6330cdd687b416d4c5
Priyabrata Sahoo <priyabrata.devops@gmail.com>
2023-09-03 21:25:39 +0530

    changes

--- 25_api_request_to_gitlab.py
+++ 25_api_request_to_gitlab.py
@@ -1,10 +1,7 @@
 import requests
 
 response = requests.get("https://gitlab.com/api/v4/users/priyabrata-devops/projects")
-
-
 my_project = response.json()
 
-print(my_project)
-#for project in my_project:
-#    print(f"Project Name: {project['name']},\nProject URL: {project['web_url']}\n")
+for project in my_project:
+    print(f"Project Name: {project['name']},\nProject URL: {project['web_url']}\n")
